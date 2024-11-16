2792dede27fb607ba90657e9614c494b154a1a1a
theavier <theavier@gmail.com>
2024-11-16 10:53:28 +0100

    added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Response, Query
-from backbone import get_pages, set_url_basic
+from backbone import get_pages, set_url_basic, get_counties, get_adventuretype
 from back_rss import create_rss
 from typing import Annotated
 from urllib.parse import unquote
@@ -16,7 +16,9 @@ async def root():
 def query(county: str = "Västra%20Götalands%20län", adventuretype: Annotated[list[str] | None, Query()] = None):
     """ retrieves adventures
     example http://localhost:7000/get?adventuretype=vandring&adventuretype=ledarskap """
-    results = get_pages(set_url_basic(county=county, _at_list=adventuretype))
+    print(f'counties: {county}')
+    print(f'adventuretypes: {adventuretype}')
+    results = get_pages(set_url_basic(_county=county, _at_list=adventuretype))
     rss = create_rss(results, title=f"Friluftsfrämjandet {unquote(county)} RSS", desc=",".join(adventuretype))
     return Response(content=rss, media_type="application/xml")
 
@@ -25,6 +27,20 @@ def gen() -> Response:
     # get county strings
     # get adventure type
     return {"message" : "generate"}
+
+@app.get("/getcounties")
+def gen() -> Response:
+    # get county strings
+    results = get_counties()
+    print(f'counties: {results}')
+    return {"message" : results}
+
+@app.get("/getadventuretype")
+def gen() -> Response:
+    # get county strings
+    results = get_adventuretype()
+    print(f'adventuretypes: {results}')
+    return {"message" : results}
 
 # to test api run
 # uvicorn main:app --reload --port 7000
---
+++ readme.md
@@ -0,0 +1,6 @@
+To run
+http://localhost:8000/get?adventuretype=vandring&adventuretype=ledarskap
+
+http://127.0.0.1:8000/get?county=Skåne&adventuretype=Kanadensare
+
+http://127.0.0.1:8000/get?adventureType=Vatten%20%26%20paddling&adventureType=Kajak&adventureType=Kanadensare&adventureType=Segling
